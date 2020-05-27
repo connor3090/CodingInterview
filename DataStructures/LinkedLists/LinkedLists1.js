cd891b0abede89c846efa5d14bc3bd2aa1369540
null null <connor3090@bellsouth.net>
2020-05-27 19:15:10 +0000

    Added fizzbuzz

--- DataStructures/LinkedLists/LinkedLists1.js
+++ DataStructures/LinkedLists/LinkedLists1.js
@@ -1,1 +1,14 @@
-console.log("This is LinkedLists1")
+ for (let i = 0; i < 20; i++) {
+   if (i%6===0) {
+     console.log("fizzbuzz");
+   }
+   else if (i%3===0) {
+     console.log("buzz");
+   }
+   else if (i%2===0) {
+     console.log("fizz");
+   }
+   else {
+     console.log(i);
+   }
+ }
